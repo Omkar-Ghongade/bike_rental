6107dd17a12312073120c5d8b6d43f0c58faf187
Omkar Subhash Ghongade <88375748+Omkar-Ghongade@users.noreply.github.com>
2023-01-12 22:22:27 +0530

    Commit - 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,82 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Clean Code',
-        home: AnimatedSplashScreen(
-            duration: 500,
-            splash: SplashScreen(),
-            nextScreen: MainScreen(),
-        )
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ListTile(
-          leading: Image.asset("assets/icons/giphy.gif",width: 350, height: 550),
-          title: Text("Ride For a Cause"),
-        )
-
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFFFFFFFF),
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                left: 80,
+                  top: 93,
+                  child: Image.asset("assets/icons/icon.png",width: 230,height: 236),
+              ),
+              Positioned(
+                  left: 22,
+                  top: 602,
+                  child: Image.asset("assets/Images/s1img.png",width: 338,height: 186)),
+              Positioned(
+                left: 24,
+                top: 350,
+                child: OutlinedButton(
+                    onPressed:() {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SecondPage()),
+                      );
+                    },
+                    child: Text("Existing User",style: TextStyle(fontSize: 20,fontFamily: 'Roboto-Bold')),
+                ),
+              ),
+              Positioned(
+                left: 24,
+                top: 420,
+                child: OutlinedButton(
+                  onPressed:() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ThirdPage()),
+                    );
+                  },
+                  // backgroundColor: Color(0xFFF50057),
+                  child: Text("New User",style: TextStyle(fontSize: 20,fontFamily: 'Roboto-Bold')),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-
-class MainScreen extends StatelessWidget{
-  const MainScreen({super.key});
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Page"),
+      ),
     );
   }
 }
